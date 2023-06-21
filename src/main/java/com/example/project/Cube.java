72d46f7f613d8d7ca1395d24b1b41512600bd73e
Benitakakama <benitakakama@gmail.com>
2023-06-21 15:43:29 -0600

    Hello

--- src/main/java/com/example/project/Cube.java
+++ src/main/java/com/example/project/Cube.java
@@ -1,12 +1,46 @@
-package com.example.project;
+import java.util.Scanner;
+public class RubiksCube {
 
-public class Cube {
+    static String [][] Face = {
+                                    {"0", "1", "2"},
+                                    {"3", "4", "5"},
+                                    {"6", "7", "8"}
+    };
 
-	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
-	//hi there
-	
-	}
+    static void showFace(String [][] face) {
+         for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++)
+                System.out.print (face[i][j]);
+            System.out.println();
+         }
+    }
 
+    static void rotateFace(String [][] face, boolean clockwise) {
+        String[][] tempface = new String [3][3];
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++)
+                tempface[i][j] = face[i][j];
+        
+        if(clockwise) {
+            for(int i = 0; i < 3; i++)
+                for(int j = 0; j < 3; j++)
+                    face[j][2-i] = tempface[i][j];
+        
+        }
+        else{
+            for(int i = 0; i < 3; i++)
+                for(int j = 0; j < 3; j++)
+                    face[2-j][i] = tempface[i][j];
+        }
+    }
+    
+
+    public static void main (String[] args) {
+
+        showFace(Face);
+        rotateFace(Face, false);
+        System.out.println();
+        showFace(Face);
+
+    }
 }
-
