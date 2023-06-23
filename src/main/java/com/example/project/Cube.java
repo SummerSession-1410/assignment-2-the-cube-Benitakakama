223dc586f4f98dddb338c51329f4de32592433de
Benitakakama <benitakakama@gmail.com>
2023-06-23 13:01:15 -0600

    Friday 23rd

--- src/main/java/com/example/project/Cube.java
+++ src/main/java/com/example/project/Cube.java
@@ -1,11 +1,39 @@
-import java.util.Scanner;
-public class RubiksCube {
+package com.example.project;
 
-    static String [][] Face = {
-                                    {"0", "1", "2"},
-                                    {"3", "4", "5"},
-                                    {"6", "7", "8"}
-    };
+public class Cube {
+	
+	static String [] [] [] cube = {
+		{
+			{"0b", "1b", "2b"},
+			{"3b", "4b", "5b"},
+			{"6b", "7b", "8b"}
+		},
+		{
+			{"0g", "1g", "2g"},
+			{"3g", "4g", "5g"},
+			{"6g", "7g", "8g"}
+		},
+		{
+			{"0r", "1r", "2r"},
+			{"3r", "4r", "5r"},
+			{"6r", "7r", "8r"}
+		},
+		{
+			{"0o", "1o", "2o"},
+			{"3o", "4o", "5o"},
+			{"6o", "7o", "8o"}
+		},
+		{
+			{"0w", "1w", "2w"},
+			{"3w", "4w", "5w"},
+			{"6w", "7w", "8w"}
+		},
+		{
+			{"0y", "1y", "2y"},
+			{"3y", "4y", "5y"},
+			{"6y", "7y", "8y"}
+		}
+	};
 
     static void showFace(String [][] face) {
          for(int i = 0; i < 3; i++) {
@@ -37,10 +65,10 @@ public class RubiksCube {
 
     public static void main (String[] args) {
 
-        showFace(Face);
-        rotateFace(Face, false);
+        showFace(cube[0]);
+        rotateFace(cube[0], false);
         System.out.println();
-        showFace(Face);
+        showFace(cube[0]);
 
     }
 }
